fa1f8588ebb35c0f8f50b4c88ab167d0c2ec6858
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-06 10:54:40 +0545

    added header component and ant design setup done

---
+++ src/components/nav/Header.jsx
@@ -0,0 +1,34 @@
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import React, { useState } from 'react';
+const items = [
+  {
+    label: 'Home',
+    key: 'mail',
+    icon: <MailOutlined />,
+  },
+  {
+    label: 'Register',
+    key: 'SubMenu',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        label: 'Option 1',
+        key: 'setting:1',
+      },
+      {
+        label: 'Option 2',
+        key: 'setting:2',
+      },
+    ],
+  },
+];
+const Header = () => {
+  const [current, setCurrent] = useState('mail');
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+};
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import "antd/dist/antd.css";
 
 ReactDOM.render(
   <React.StrictMode>
