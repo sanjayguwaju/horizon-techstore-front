00030719703e9c8d9e22b743e346b5ce01f76f05
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-06 15:16:09 +0545

    register form created

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Register = () => {
-  return (
-    <div>
-      <p>Register</p>
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
 
-export default Register
+  const handleSubmit = () => {
+    //
+  };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        autoFocus
+      />
+
+      <button type="submit" className="btn btn-raised">
+        Register
+      </button>
+    </form>
+  );
+
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          {registerForm()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
