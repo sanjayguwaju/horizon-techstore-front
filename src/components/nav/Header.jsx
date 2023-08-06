bb8cdada0d60bcec0c2a0d6f08261e0ec32b045a
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-06 11:19:58 +0545

    removed const itemes and used each item in return as component

--- src/components/nav/Header.jsx
+++ src/components/nav/Header.jsx
@@ -1,44 +1,31 @@
 import { AppstoreOutlined, MailOutlined, SettingOutlined, UserAddOutlined, UserOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
 import React, { useState } from 'react';
-const items = [
-  {
-    label: 'Home',
-    key: 'home',
-    icon: <AppstoreOutlined />,
-  },
-  {
-    label: 'Login',
-    key: 'login',
-    icon: <UserOutlined />,
-  },
-  {
-    label: 'Register',
-    key: 'register',
-    icon: <UserAddOutlined />,
-  },
-  {
-    label: 'Username',
-    key: 'SubMenu',
-    icon: <SettingOutlined />,
-    children: [
-      {
-        label: 'Option 1',
-        key: 'setting:1',
-      },
-      {
-        label: 'Option 2',
-        key: 'setting:2',
-      },
-    ],
-  },
-];
+
 const Header = () => {
   const [current, setCurrent] = useState('home');
+
   const onClick = (e) => {
-    console.log('click ', e);
     setCurrent(e.key);
   };
-  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+
+  return (
+    <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal">
+      <Menu.Item key="home" icon={<AppstoreOutlined />}>
+        Home
+      </Menu.Item>
+      <Menu.Item key="login" icon={<UserOutlined />} className="float-right">
+        Login
+      </Menu.Item>
+      <Menu.Item key="register" icon={<UserAddOutlined />} className="float-right">
+        Register
+      </Menu.Item>
+      <Menu.SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
+        <Menu.Item key="setting:1">Option 1</Menu.Item>
+        <Menu.Item key="setting:2">Option 2</Menu.Item>
+      </Menu.SubMenu>
+    </Menu>
+  );
 };
+
 export default Header;
