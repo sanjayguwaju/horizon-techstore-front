84087c2c2efa3fb46b1464231c5413ccdf57378c
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-06 15:16:01 +0545

    Link added to the nav items

--- src/components/nav/Header.jsx
+++ src/components/nav/Header.jsx
@@ -1,6 +1,8 @@
 import { AppstoreOutlined, MailOutlined, SettingOutlined, UserAddOutlined, UserOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
 import React, { useState } from 'react';
+import {Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
   const [current, setCurrent] = useState('home');
@@ -12,13 +14,16 @@ const Header = () => {
   return (
     <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal">
       <Menu.Item key="home" icon={<AppstoreOutlined />}>
-        Home
+        <Link to="/">Home</Link>
       </Menu.Item>
-      <Menu.Item key="login" icon={<UserOutlined />} className="float-right">
-        Login
-      </Menu.Item>
+      <div>
       <Menu.Item key="register" icon={<UserAddOutlined />} className="float-right">
-        Register
+        <Link to="/register">Register</Link>
+      </Menu.Item>
+
+      </div>
+      <Menu.Item key="login" icon={<UserOutlined />} className="float-right">
+        <Link to="/login">Login</Link>
       </Menu.Item>
       <Menu.SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
         <Menu.Item key="setting:1">Option 1</Menu.Item>
