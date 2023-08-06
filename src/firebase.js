5fa8305f33d3ab46d0909a7946fd5364c68a69fd
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-06 19:21:49 +0545

    auth is already declared so it is removed from the code

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp,auth } from "firebase/app";
+import { initializeApp} from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
