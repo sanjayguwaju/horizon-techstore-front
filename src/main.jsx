19ae6b21f3a59362b6d8df904d4b610f099984d6
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-26 21:16:18 +0545

    feat: redux setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import "antd/dist/antd.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from '../src/pages/reducers';
+
+// store 
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
+  <Provider store={store}>
     <BrowserRouter>
       <App />
-    </BrowserRouter>,
+    </BrowserRouter>
+    </Provider>,
   document.getElementById('root')
 );
