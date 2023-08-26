ebbdcae94b1aa91485ee7a05123763e26922ed35
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-26 20:41:15 +0545

    fix: react importe which we don't need

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {auth} from '../../firebase';
 import {sendSignInLinkToEmail } from "firebase/auth";
 import {toast} from 'react-toastify';
