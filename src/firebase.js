792d9972c785c08368c9efe197a6fc3ce3160a17
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-27 18:29:10 +0545

    fix: addeed importing firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 import { initializeApp} from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
