b29b4d97ccf997d6f779b00724ef061732c88491
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-27 18:29:46 +0545

    fix: added logout functionality

--- src/components/nav/Header.jsx
+++ src/components/nav/Header.jsx
@@ -1,34 +1,60 @@
-import { AppstoreOutlined, MailOutlined, SettingOutlined, UserAddOutlined, UserOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
-import React, { useState } from 'react';
-import {Link } from 'react-router-dom';
-import './Header.css';
+import { useState } from "react";
+import { Menu } from "antd";
+import {
+  AppstoreOutlined,
+  SettingOutlined,
+  UserOutlined,
+  UserAddOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+const { SubMenu, Item } = Menu;
 
 const Header = () => {
-  const [current, setCurrent] = useState('home');
+  const [current, setCurrent] = useState("home");
 
-  const onClick = (e) => {
+  let dispatch = useDispatch();
+  let history = useHistory();
+
+  const handleClick = (e) => {
+    console.log(e.key);
     setCurrent(e.key);
   };
 
-  return (
-    <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal">
-      <Menu.Item key="home" icon={<AppstoreOutlined />}>
-        <Link to="/">Home</Link>
-      </Menu.Item>
-      <div>
-      <Menu.Item key="register" icon={<UserAddOutlined />} className="float-right">
-        <Link to="/register">Register</Link>
-      </Menu.Item>
+  const logout = () => {
+    dispatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+    history.push("/login");
+  };
 
-      </div>
-      <Menu.Item key="login" icon={<UserOutlined />} className="float-right">
+  return (
+    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+      <Item key="home" icon={<AppstoreOutlined />}>
+        <Link to="/">Home</Link>
+      </Item>
+
+      <Item key="register" icon={<UserAddOutlined />} className="float-right">
+        <Link to="/register">Register</Link>
+      </Item>
+
+      <Item key="login" icon={<UserOutlined />} className="float-right">
         <Link to="/login">Login</Link>
-      </Menu.Item>
-      <Menu.SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
-        <Menu.Item key="setting:1">Option 1</Menu.Item>
-        <Menu.Item key="setting:2">Option 2</Menu.Item>
-      </Menu.SubMenu>
+      </Item>
+
+      <SubMenu icon={<SettingOutlined />} title="Username">
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
+        <Item icon={<LogoutOutlined />} onClick={logout}>
+          Logout
+        </Item>
+      </SubMenu>
     </Menu>
   );
 };
