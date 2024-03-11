8d47809bb38e511c64a364207d9e2e658fbe7769
sanjayguwaju <sanjayguwaju@gmail.com>
2024-03-11 18:12:37 +0545

    product card component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getProductsByCount } from "../functions/product";
+import ProductCard from "../components/cards/ProductCard";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -13,16 +14,28 @@ const Home = () => {
     try {
       const res = await getProductsByCount(3);
       setProducts(res);
+      setLoading(false);
     } catch (err) {
       console.error(err);
     }
   };
 
   return (
-    <div>
-      <p>react home</p>
-      {JSON.stringify(products)}
-    </div>
+    <>
+      <div className="jumbotron">
+        {loading ? <h4>Loading...</h4> : <h4>All Products</h4>}
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {products.map((product) => (
+            <div key={product._id} className="col-md-4">
+              <ProductCard product={product} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
