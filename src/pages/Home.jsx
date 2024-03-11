750e010b7060db86545f8f805acc25e21825fd39
sanjayguwaju <sanjayguwaju@gmail.com>
2024-03-11 18:07:58 +0545

    fetch products in a homepage completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { getProductsByCount } from "../functions/product";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = async () => {
+    try {
+      const res = await getProductsByCount(3);
+      setProducts(res);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div>
-      <p>Home</p>
+      <p>react home</p>
+      {JSON.stringify(products)}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
